1cc490497858dbe16afc55d91c0d4ba5285940e6
anant basu <anantbasu85073@gmail.com>
2023-12-07 15:52:34 +0530

    My TestSecond Commit

--- app/src/main/java/com/example/horseriding/ui/Demo.java
+++ app/src/main/java/com/example/horseriding/ui/Demo.java
@@ -1,10 +1,7 @@
 package com.example.horseriding.ui;
 
-<<<<<<< HEAD
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Demo extends AppCompatActivity {
-=======
-public class Demo {
->>>>>>> origin/Anant2
+
 }
