a28dad76343f82e0f1b0aea73d7a8640b5ebeb5e
anant basu <anantbasu85073@gmail.com>
2023-12-07 16:18:01 +0530

    Five commit

--- app/src/main/java/com/example/horseriding/ui/Demo.java
+++
@@ -1,7 +0,0 @@
-package com.example.horseriding.ui;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class Demo extends AppCompatActivity {
-
-}
--- app/src/main/java/com/example/horseriding/ui/KotlinClass.java
+++
@@ -1,4 +0,0 @@
-package com.example.horseriding.ui;
-
-public class KotlinClass {
-}
