8f0502507e81ef11dc196220b3f391a6dd3d81dd
anant basu <anantbasu85073@gmail.com>
2023-12-08 16:45:31 +0530

    Eleven commit

---
+++ app/src/main/java/com/example/horseriding/modal/RupeeModel.java
@@ -0,0 +1,18 @@
+package com.example.horseriding.modal;
+
+public class RupeeModel {
+
+    private String rupeeText;
+
+    public RupeeModel(String rupeeText) {
+        this.rupeeText = rupeeText;
+    }
+
+    public String getRupeeText() {
+        return rupeeText;
+    }
+
+    public void setRupeeText(String rupeeText) {
+        this.rupeeText = rupeeText;
+    }
+}
